3777c0e0149fd03770b0cb65eaff5e1f26ec476a
Abdul-Qoyyum <nurudeen96qoyyum@gmail.com>
2020-04-30 01:08:27 +0000

    added admin layout page

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,22 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .styles([
+      'resources/assets/css/blog-post.css',
+      'resources/assets/css/bootstrap.css',
+      'resources/assets/css/bootstrap.min.css',
+      'resources/assets/css/font-awesome.css',
+      'resources/assets/css/metisMenu.css',
+      'resources/assets/css/sb-admin-2.css',
+      'resources/assets/css/styles.css',
+      'resources/assets/css/timeline.css'
+     ],'public/css/all.css')
+     .scripts([
+      'resources/assets/js/bootstrap.js',
+      'resources/assets/js/jquery.js',
+      'resources/assets/js/sb-admin-2.js',
+      'resources/assets/js/bootstrap.min.js',
+      'resources/assets/js/metisMenu.js'
+     ],'public/js/all.js');
+
